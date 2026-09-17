8a38a9a89a39ce9f3913ec0e504a10212824ab9b
Nisha Paulraj Mahendran <nishapm2007@gmail.com>
2026-09-17 21:50:32 +0530

    Create

---
+++ Arrays/problem-1789662031459/problem-1789662031459.py
@@ -0,0 +1,15 @@
+# Last updated: 9/17/2026, 9:50:31 PM
+1class Solution:
+2    def merge(self, intervals: list[list[int]]) -> list[list[int]]:
+3        intervals.sort(key=lambda x:x[0])
+4
+5        result=[intervals[0]]
+6
+7        for start,end in intervals[1:]:
+8            prevInterval=result[-1]
+9
+10            if start<=prevInterval[1]:
+11                result[-1][1]=max(end,prevInterval[1])
+12            else:
+13                result.append([start,end])
+14        return result 
